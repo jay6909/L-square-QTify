82ad7d2d5fd9c62463af10b4233b9de1a178d660
Jayaditya G <gaikwadjayaditya@gmail.com>
2023-11-07 16:49:24 +0530

    hero-qtify-takehome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Hero from './components/Hero';
 import NavBar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Hero/>
     </div>
   );
 }
